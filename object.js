9e5187c02b090aa8ee5bc358561ff32ce7db7260
Fatuma <sfatex@gmail.com>
2021-07-15 00:09:17 -0400

    made added toppings

--- object.js
+++ object.js
@@ -1,5 +1,5 @@
 const pizza = {  
-       toppings: ['cheese', 'sauce', 'pepperoni'],  
+       toppings: ['cheese', 'sauce', 'pepperoni','mushroom'],  
        crust: 'deep dish',  
        serves: 2  
      }  
